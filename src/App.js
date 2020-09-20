5f73b9c39061e879caf18e0d82365a8b9dceaa8d
Turkay Tunc <turkaytunc00@gmail.com>
2020-09-20 15:47:47 +0300

    add hello component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Hello } from './components/hello';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Travis</h1>
+      <Hello />
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Hello = () => {
+  return (
+    <div>
+      <h1>Hello Travis</h1>
+      <h2>Deploy via travis</h2>
+    </div>
+  );
+};
