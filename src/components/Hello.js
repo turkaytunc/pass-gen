3667aaf60c31323ec5f5701082ca2cc8017ecbc5
Turkay Tunc <turkaytunc00@gmail.com>
2020-09-24 08:30:39 +0300

    add uppercase generator

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { generatePassword } from '../util/generate-password';
 
 export const Hello = () => {
+  console.log(generatePassword(3, true));
   return (
     <div>
       <h1>Hello Travis</h1>
--- src/util/generate-password.js
+++ src/util/generate-password.js
@@ -8,23 +8,24 @@ export const generatePassword = (
 ) => {
   if (length <= 0) return 0;
   let funcArr = [];
-  let result = '';
   if (isUpper) funcArr.push(generateUppercase);
   if (isLower) funcArr.push(generateLowercase);
   if (isPunctuation) funcArr.push(generatePunctuation);
   if (funcArr.length <= 0) return 0;
 
+  let result = '';
   for (let i = 0; i < length; i++) {
     const rand = generateRandomNumber(0, funcArr.length - 1);
 
-    result.concat(funcArr[rand]());
+    result = result.concat(funcArr[rand]());
   }
+
   console.log(result);
   return result;
 };
 
 export const generateUppercase = () => {
-  const rand = generateRandomNumber(65, 97);
+  const rand = generateRandomNumber(65, 90);
 
   return String.fromCharCode(rand);
 };
